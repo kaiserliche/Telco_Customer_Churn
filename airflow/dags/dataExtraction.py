333ba9c3793c10113b20b3ca7eaf5ea1398bac2c
Anubhav Kumar <anubhav22.kumar@gmail.com>
2024-10-27 21:53:29 +0530

    Update dataExtraction.py

--- airflow/dags/dataExtraction.py
+++ airflow/dags/dataExtraction.py
@@ -12,10 +12,9 @@ class Config:
     DOWNLOAD_DIR = "../data"  # Updated to absolute path
     ZIP_FILE_PATH = os.path.join(DOWNLOAD_DIR, "dataset.zip")
     # DB_CONN = "postgres_default"
-    API_CONN_ID = "Telco"  # Ensure 'Telco' connection is defined in Airflow
-    HOST = "https://storage.googleapis.com"
-    END_POINT = "/kaggle-data-sets/1020460/1720381/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20241023%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20241023T092408Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=c8b93fa3ee3ca7322f7cf3bf34ee060ccad95dfc640b33b80ba9f5653923be784089e7a68ada612a0d23e2be2166b2a55a866722aa1789c14126dfeb111bce14822004c6d7c723fe85fdd30f75b5b1a64ac07cc342d2f8bfc9f1421b487d0e11c3e03bfbe3c5255e3babbd8fc8cbeecba2ed88f734a65a04dcc695d35b46206d04a76f31022b474af56d2960831134882b392c0aa68ccdf21807e8b7001e4d757c94854d4aa875ea4337bc8c54028e49aeb0cda8727731018ef94a27fd7acce8df78791a63b5750db9e9a6c14a2f5f17c8753743e35d8776deb970a3125180b4903f7648ccf8120798127163faf64dbacff17fce2328aa063aa16eccdd311f85"
-
+    API_CONN_ID = "Telco_Kaggle"  # Ensure 'Telco' connection is defined in Airflow
+    HOST = "https://www.kaggle.com"
+    END_POINT = "/api/v1/datasets/download/yeanzc/telco-customer-churn-ibm-dataset"
 
 # Logging setup
 logger = logging.getLogger(__name__)
